2e5215b705452a0dd9c27cd4b119e948de0eb491
LabiadWeb <labiadmo920@gmail.com>
2024-04-06 03:38:27 +0200

    labiad

---
+++ Pharmacie-Management-System/src/main/java/example/controller/UtilisateurController.java
@@ -0,0 +1,15 @@
+package example.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UtilisateurController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
