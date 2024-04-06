b81d116d1a4f6ebdd4e61cfcc9eee7089bcfef0f
rrelma <elmahirachid24@gmail.com>
2024-04-06 01:12:14 +0000

    star

---
+++ Pharmacie-Management-System/src/main/java/example/controller/Produit_controller.java
@@ -0,0 +1,15 @@
+package example.controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Produit_controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
