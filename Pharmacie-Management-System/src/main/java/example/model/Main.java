1584840a2fbfd9e051aa6e1505f88986bfedf452
LabiadWeb <labiadmo920@gmail.com>
2024-04-08 05:00:55 +0200

    labiad

--- Pharmacie-Management-System/src/main/java/example/model/Main.java
+++ Pharmacie-Management-System/src/main/java/example/model/Main.java
@@ -15,9 +15,6 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/Main.fxml"));
         double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
         double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
-        if(screenWidth>1100){
-            screenWidth = 1100;
-        }
         System.out.print("Width = "+screenWidth+" Height = "+screenHeight);
         Scene scene = new Scene(fxmlLoader.load(), screenWidth, screenHeight);
         stage.setTitle("Hello!");
