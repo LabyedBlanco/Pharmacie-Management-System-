2ec15695bcf0db100dff36acc7b3bea9ba7f164a
Bdiro23 <zirarbe@gmail.com>
2024-04-13 00:05:08 +0000

    pedro2

--- Pharmacie-Management-System/src/main/java/example/controller/LoginController.java
+++ Pharmacie-Management-System/src/main/java/example/controller/LoginController.java
@@ -1,13 +1,31 @@
 package example.controller;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void section(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- Pharmacie-Management-System/src/main/java/example/controller/UtilisateurController.java
+++ Pharmacie-Management-System/src/main/java/example/controller/UtilisateurController.java
@@ -1,17 +1,24 @@
 package example.controller;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import java.io.File;
 import java.io.IOException;
 
+
 public class UtilisateurController extends Application {
-    public BorderPane scene1;
+
+    public Button bottonmodify;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -25,10 +32,28 @@ public class UtilisateurController extends Application {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Profile_utilisateur.fxml"));
         Parent root = loader.load();
+        UtilisateurController controller = loader.getController();
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.setTitle("Add a User");
+        stage.setResizable(false);
         stage.show();
+        controller.bottonmodify.setVisible(false);
 
     }
+
+    public void onaddimageUser(ActionEvent event){
+
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image File");
+
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file != null) {
+
+        }
+    }
+
+
+
 }
