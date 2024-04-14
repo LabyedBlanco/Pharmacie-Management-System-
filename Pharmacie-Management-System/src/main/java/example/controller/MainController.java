f548707508511e00774b9db3da4271dfae329458
LabiadWeb <labiadmo920@gmail.com>
2024-04-14 01:04:25 +0000

    laibad

--- Pharmacie-Management-System/src/main/java/example/controller/MainController.java
+++ Pharmacie-Management-System/src/main/java/example/controller/MainController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -18,6 +19,8 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
     @FXML
     private GridPane MainPane;
+    @FXML
+    private AnchorPane Display;
 
     @FXML
     private Button comm;
@@ -42,6 +45,7 @@ public class MainController implements Initializable {
 
 
     public void initialize(URL url , ResourceBundle resourceBundle) {
+        MainPane.getChildren().clear();
         dash.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         prod.setStyle("-fx-background-color: transparent");
         vent.setStyle("-fx-background-color: transparent");
@@ -56,15 +60,17 @@ public class MainController implements Initializable {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
     }
 
 
     public void OnProduit (ActionEvent event) throws IOException {
+
+        MainPane.getChildren().clear();
         System.out.print("\nGestion des Produit");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Produits");
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
         prod.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         dash.setStyle("-fx-background-color: transparent");
         vent.setStyle("-fx-background-color: transparent");
@@ -72,29 +78,29 @@ public class MainController implements Initializable {
         comm.setStyle("-fx-background-color: transparent");
         four.setStyle("-fx-background-color: transparent");
 
+
+
     }
 
     public void OnUtilisateur (ActionEvent event) throws IOException {
-
+        MainPane.getChildren().clear();
         System.out.print("\nGestion des Utilisateurs");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Utilisateur");
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
         util.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         dash.setStyle("-fx-background-color: transparent");
         vent.setStyle("-fx-background-color: transparent");
         comm.setStyle("-fx-background-color: transparent");
         prod.setStyle("-fx-background-color: transparent");
         four.setStyle("-fx-background-color: transparent");
-
-
     }
     public void OnDashboard (ActionEvent event) throws IOException {
-
+        MainPane.getChildren().clear();
         System.out.print("\nDashboard");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Dashboard");
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
         dash.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         comm.setStyle("-fx-background-color: transparent");
         vent.setStyle("-fx-background-color: transparent");
@@ -106,11 +112,11 @@ public class MainController implements Initializable {
     }
 
     public void OnCommande (ActionEvent event) throws IOException {
-
+        MainPane.getChildren().clear();
         System.out.print("\nCommande");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Commande");
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
         comm.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         dash.setStyle("-fx-background-color: transparent");
         vent.setStyle("-fx-background-color: transparent");
@@ -121,11 +127,11 @@ public class MainController implements Initializable {
 
     }
     public void OnVentes (ActionEvent event) throws IOException {
-
+        MainPane.getChildren().clear();
         System.out.print("\nVentes");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Ventes");
-        MainPane.add(view,1,0);
+        MainPane.add(view,0,0);
         vent.setStyle("-fx-background-color: transparent;-fx-effect: dropshadow(gaussian, white, 10, 0.05, 0, 0);-fx-border-color: WHITE; -fx-border-width: 0px 0px 0px 3px;");
         dash.setStyle("-fx-background-color: transparent");
         comm.setStyle("-fx-background-color: transparent");
@@ -136,7 +142,7 @@ public class MainController implements Initializable {
 
     }
     public void OnFournisseur (ActionEvent event) throws IOException {
-
+        MainPane.getChildren().clear();
         System.out.print("\nFournisseur");
         FxmlLoader object = new FxmlLoader();
         Pane view = object.setPage("Fournisseur");
@@ -147,6 +153,5 @@ public class MainController implements Initializable {
         util.setStyle("-fx-background-color: transparent");
         comm.setStyle("-fx-background-color: transparent");
         prod.setStyle("-fx-background-color: transparent");
-
     }
 }
