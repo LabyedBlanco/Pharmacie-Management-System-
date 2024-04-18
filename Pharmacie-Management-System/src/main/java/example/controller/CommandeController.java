53879c4afcc3c4a611a3c2ebbdbab51e804046e6
LabiadWeb <labiadmo920@gmail.com>
2024-04-18 15:30:36 +0100

    labiad

--- Pharmacie-Management-System/src/main/java/example/controller/CommandeController.java
+++ Pharmacie-Management-System/src/main/java/example/controller/CommandeController.java
@@ -6,13 +6,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class CommandeController implements Initializable {
@@ -20,6 +26,9 @@ public class CommandeController implements Initializable {
     @FXML
     private ComboBox<String> Selectbox;
 
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
     @FXML
     private Text main;
 
@@ -29,9 +38,55 @@ public class CommandeController implements Initializable {
     @FXML
     private GridPane Commande;
 
+    @FXML
+    private TextField AjProduit;
+
+    @FXML
+    private Button ComConfirm;
+
+    @FXML
+    private DatePicker ComDate;
+
+    @FXML
+    private TextField ComDetails;
+
+    @FXML
+    private TextField ComFournisseur;
+
+    @FXML
+    private TextField ComQantite;
+
+
+    @FXML
+    private ComboBox<String> Depot;
+
+    @FXML
+    private GridPane MainPane;
+    @FXML
+    private ComboBox<String> ComPayement;
+
+
+
+
 
     @Override
     public void initialize(URL url , ResourceBundle resourceBundle){
+        if(Depot != null){
+            Depot.setItems(FXCollections.observableArrayList("1","2","3","4"));
+            Depot.setOnAction(e -> {
+                String selectedOption = Depot.getValue();
+                System.out.println(selectedOption);
+            });
+        }
+
+        if(ComPayement != null){
+            ComPayement.setItems(FXCollections.observableArrayList("Cash","Check","Carte Bancaire"));
+            ComPayement.setOnAction(e -> {
+                String selectedOption = ComPayement.getValue();
+                System.out.println(selectedOption);
+            });
+        }
+
         if (Selectbox != null) {
             Selectbox.setItems(FXCollections.observableArrayList("Gestion des Commandes", "Gestion des Avoirs"));
             Selectbox.setOnAction(e -> {
