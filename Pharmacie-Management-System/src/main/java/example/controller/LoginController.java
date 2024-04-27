83439050e749100ee9d3982e04a9fd4b9d5fb1d0
LabiadWeb <labiadmo920@gmail.com>
2024-04-27 23:30:48 +0100

    labiad

--- Pharmacie-Management-System/src/main/java/example/controller/LoginController.java
+++ Pharmacie-Management-System/src/main/java/example/controller/LoginController.java
@@ -1,12 +1,14 @@
 package example.controller;
 
 import example.model.DatabaseManager;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -20,29 +22,24 @@ public class LoginController implements Initializable {
     private Scene scene;
     @FXML
     private AnchorPane Connected;
+    @FXML
+    private ComboBox Role;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            DatabaseManager Data = new DatabaseManager();
-            boolean isConnected = Data.ConnectionStat();
-            if (isConnected) {
-                System.out.print(isConnected);
-                Connected.setStyle("-fx-background-color: green; -fx-background-radius: 100px");
-            } else {
-                System.out.print(isConnected);
-                Connected.setStyle("-fx-background-color: red; -fx-background-radius: 100px");
-            }
-        } catch (Exception e) {
-            System.err.println("Error initializing connection: " + e.getMessage());
+        if (Role != null) {
+            Role.setItems(FXCollections.observableArrayList("Gérant","Pharmacien", "Admin"));
         }
     }
 
-    public void section(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        public void section (ActionEvent event) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            stage.setResizable(false);
+        }
     }
-}
+
